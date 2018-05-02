e99fbdbcf7cb9a8ceff335d9a71c3ee2bbc123af
JacobGarcia <fatalraincloud@gmail.com>
2018-05-02 12:21:04 -0500

    Added configuration file

---
+++ modules/config.php
@@ -0,0 +1,7 @@
+<?php
+	// Variables Globales
+	$site_url = 'https://iceberg9.com/playground/face-id/';
+	//$site_url = 'https://frm.kawlantid.com/';
+	$rol = $_SESSION['rol'];
+	$token = $_SESSION['token'];
+?>
