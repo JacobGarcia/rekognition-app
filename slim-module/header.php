762319f5ed3d5ec6dbf4dc2c4610be27bc9c9291
JacobGarcia <fatalraincloud@gmail.com>
2018-05-03 14:09:21 -0500

    Removing shit reference

--- slim-module/header.php
+++ slim-module/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Slim Module</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
-	<link rel="stylesheet" href="<?php print $site_url; ?>/style.css">
+	<link rel="stylesheet" href="/style.css">
 </head>
 <body>
 	<div class="page__wrapper">
