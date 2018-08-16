2dc98863ef3914ff6600aa990951e74996a28d20
Jacob García <rock2494@hotmail.com>
2018-08-16 16:16:17 -0500

    Changed references to connus.io

--- slim-module/config.php
+++ slim-module/config.php
@@ -1,6 +1,6 @@
 <?php
 	// Variables Globales
-	$site_url = 'http://localhost:8888/slim-module';
+	$site_url = 'http://connus.io/slim-module';
 	$token_hardcode = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTZiZGMwZDE3NjI2MmVmMjQ3OGFkZTIiLCJhY2MiOjQsImNtcCI6IjVhMTg5OGUyZjQwMzZhMzM5NzI2ZGJhZSIsImlhdCI6MTUyMDg3Nzc4MH0.n8wm8wFj9CO3l-kF4Ka-OlBicRZUsyXf6FcmiJxKqXo';
 	//$site_url = 'https://frm.kawlantid.com/';
 	//$rol = $_SESSION['rol'];
